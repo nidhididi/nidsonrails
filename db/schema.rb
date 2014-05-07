678fe52b1a38db538a5630302d1fcd06d278c6c6
Nidhi Mehta <nidhididi@gmail.com>
2014-05-06 19:45:26 -0500

    added gallery images

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140429034725) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: true do |t|
     t.string   "title"
     t.text     "blog_content"
